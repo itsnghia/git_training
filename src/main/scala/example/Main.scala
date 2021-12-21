338e333e5c4ca5dee737c916dbc6d9bc377dacfb
itsnghia <nghia14041999@gmail.com>
2021-12-21 22:09:53 +0700

    project updated

---
+++ src/main/scala/example/Main.scala
@@ -0,0 +1,19 @@
+package example
+
+import scala.collection.mutable.ListBuffer
+
+object Main extends App {
+  // Type safe exercise
+  def printTypeSafe(x: Int): Unit = {
+    var nums = ListBuffer[Any]()
+    for (i <- 1 to x) {
+      i match {
+        case i if i % 3 == 0 && i % 5 == 0 => nums += "typesafe"
+        case i if i % 3 == 0 => nums += "type"
+        case i if i % 5 == 0 => nums += "safe"
+        case _ => nums += i
+      }
+    }
+  }
+}
+
